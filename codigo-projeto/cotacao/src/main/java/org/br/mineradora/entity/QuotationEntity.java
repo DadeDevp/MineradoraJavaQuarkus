c6e16ef4be2a1a76ed20c142b01f98e796c88a0d
David Bittencourt <devp.dade@gmail.com>
2023-11-14 20:15:28 -0300

    Criado Cotacao MicroServico, adicionada biblioteca lombok e QuotationEntity

---
+++ codigo-projeto/cotacao/src/main/java/org/br/mineradora/entity/QuotationEntity.java
@@ -0,0 +1,33 @@
+package org.br.mineradora.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "quotation")
+@Data
+@NoArgsConstructor
+public class QuotationEntity {
+
+    @Id
+    @GeneratedValue //inidicar que cada elemento inserido no banco o id seja incrementado em 1
+    private Long id;
+
+    private Date date;
+
+    @Column(name = "currencyPrice") // forçar que a coluna seja nomeada em camelCase
+    private BigDecimal currencyPrice;
+
+    //porcentagem de mudança dos preços desde a ultima busca
+    @Column(name = "pctChange")
+    private String pctChange;
+
+    //moedas comparadas
+    private String pair;
+
+}
