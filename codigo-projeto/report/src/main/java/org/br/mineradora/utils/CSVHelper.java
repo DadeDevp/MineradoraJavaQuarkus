0158739d42580d22e1a6b175b69c1159ccd29839
David Bittencourt <devp.dade@gmail.com>
2023-11-30 18:24:01 -0300

    Criacao das classes OpportunityEntity, QuotationEntity, OpportunityDTO, ProposalDTO, QuotationDTO e inicio da classe CSVHelper

---
+++ codigo-projeto/report/src/main/java/org/br/mineradora/utils/CSVHelper.java
@@ -0,0 +1,19 @@
+package org.br.mineradora.utils;
+
+import org.apache.commons.csv.CSVFormat;
+import org.br.mineradora.dto.OpportunityDTO;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+public class CSVHelper {
+
+    public static ByteArrayInputStream
+    opportunitiesToCSV(List<OpportunityDTO> opportunities){
+
+        final CSVFormat format = CSVFormat.DEFAULT.withHeader("ID Proposta", "Cliente", "Preço por Tonelada", "Melhor cotação");
+
+        
+    }
+
+}
